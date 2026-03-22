755d4203ccf8932f694348e5867832a6648d4585
h12t20 <sergdiamond@ymail.com>
2026-03-22 23:29:39 +0300

    some refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Test from './Test';
 
-import './index.output.css';
+import './index.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
